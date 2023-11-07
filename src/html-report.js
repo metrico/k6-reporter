8562b837cadead4cc0a7f35758f561813c097342
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2023-11-07 22:54:11 +0100

    Update html-report.js

--- src/html-report.js
+++ src/html-report.js
@@ -9,7 +9,7 @@
 // Have to import ejs this way, nothing else works
 import ejs from '../node_modules/ejs/ejs.min.js'
 import template from './template.ejs'
-import md_template from './markdown.ejs'
+import mdtemplate from './markdown.ejs'
 
 const version = '2.3.1'
 
@@ -198,7 +198,7 @@ export function markdownReport(data, opts = {}) {
   ]
 
   // Render the markdown template
-  const markdown = ejs.render(md_template, {
+  const markdown = ejs.render(mdtemplate, {
     data,
     title: opts.title,
     standardMetrics,
